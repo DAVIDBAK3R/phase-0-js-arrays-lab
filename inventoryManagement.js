2ccc0aaddfb7746018bcfb131bfe945c33d6a230
Baker <davekaris40@gmail.com>
2025-11-24 12:06:45 +0300

    inventoryManagement.js

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,17 @@
-// Write your code here
+let products = ["Laptop","Phone","Headphones","Monitor"]
 
+function logFirstProduct(){
+  console.log(products[0]);
+}
+ function addProduct(Tablet){
+   products.push(Tablet);
+ }
 
+function updateProductName(position, newName)
+{
+  products[position] = newName;
+}
+updateProductName(1,"Smartphone");
 
-// Export the necessary parts for testing
-module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
-};
+function removeLastProduct(){
+  products.pop(
